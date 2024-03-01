d6dafbaaa6b0e52fa0071ec209a1c4bf331b18e2
ajmalMundumuzhi <149889538+ajmalMundumuzhi@users.noreply.github.com>
2024-03-01 21:30:39 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,17 +4,16 @@ const state={
     db:null
 }
 module.exports.connect=async function(done){
-    const url='mongodb://127.0.0.1:27017/'
+    const url='mongodb+srv://muhajmal73:mongodb_ajmal_server@cluster0.pwtvatv.mongodb.net/?retryWrites=true&w=majority'
     const dbname='Limelight'
 
-     const client = await mongoClient.connect(url)
+    const client = await mongoClient.connect(url)
     state.db=client.db(dbname)
 }
 
     module.exports.get=function(){
         
       if(!state.db) {
-          this.connect()
           console.log('Database Not connected');
       }
         return state.db
